3c34a0855e69033648a68b86f16399f92fcb2597
marcosviniciusdev3 <marcos.vinicius.dev@outlook.com>
2026-08-16 06:46:05 -0300

    Add AKS ACI

--- clusters/aks-cluster01/main.tf
+++ clusters/aks-cluster01/main.tf
@@ -11,11 +11,37 @@ resource "azurerm_virtual_network" "aks_cluster01" {
   address_space       = ["10.0.0.0/16"]
 }
 
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.aks_cluster01.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "aci_subnet" {
+  name                 = "aci-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.aks_cluster01.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "aci-delegation"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
 resource "azurerm_kubernetes_cluster" "aks_cluster01" {
   name                = "aks-cluster01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "aks-cluster01"
+
+  node_provisioning_profile {
+    mode = "Manual"
+  }
 
   default_node_pool {
     name       = "systempool"
@@ -23,10 +49,19 @@ resource "azurerm_kubernetes_cluster" "aks_cluster01" {
     vm_size    = "Standard_B2s"
     os_sku     = "Ubuntu"
     only_critical_addons_enabled  = true
+    vnet_subnet_id                = azurerm_subnet.aks_subnet.id
   }
 
   identity {
     type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+  aci_connector_linux {
+    subnet_name = azurerm_subnet.aci_subnet.name
   }
   
   tags = {
@@ -39,9 +74,16 @@ resource "azurerm_kubernetes_cluster_node_pool" "devpool" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster01.id
   vm_size               = "Standard_B2s"
   node_count            = 1
+  vnet_subnet_id        = azurerm_subnet.aks_subnet.id
 
   node_labels = {
     environment = "development"
   }
+}
+
+resource "azurerm_role_assignment" "aks_aci_network" {
+  scope                = azurerm_subnet.aci_subnet.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_kubernetes_cluster.aks_cluster01.identity[0].principal_id
 }
 
